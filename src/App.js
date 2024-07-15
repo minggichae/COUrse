6ef78fb21ee410b851814ed9784bd8805597760b
minggichae <che08180@naver.com>
2024-07-15 20:13:54 +0900

    fix: import React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
 import Layout from "./components/layout/Layout";
 
 function App() {
-  return <Layout>{/* // router 처리를 할 공간 */}</Layout>;
+  return (
+    <>
+      <Layout>{/* // router 처리를 할 공간 */}</Layout>
+    </>
+  );
 }
 
 export default App;
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,16 @@
+import React from "react";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
+import Search from "../search/search";
 import "./Layout.css";
 
 const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Header />
-      <div className="main">컴포넌트가 들어갈 공간</div>
+      <div className="main">
+        <Search />
+      </div>
       <Footer />
     </div>
   );
